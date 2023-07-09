a9722a20170ac0a7f4f26b426b7f6ab1dd8dfe35
Alejandro Mira <alejo890327@gmail.com>
2023-07-09 11:45:33 -0500

    funcionalidad para configurar bd

--- src/main/java/com/inte_soft/gestionconsumibles/GestionConsumibles.java
+++ src/main/java/com/inte_soft/gestionconsumibles/GestionConsumibles.java
@@ -7,11 +7,18 @@ package com.inte_soft.gestionconsumibles;
 
 import com.inte_soft.gestionconsumibles.formularios.Login;
 import com.inte_soft.gestionconsumibles.formularios.SplashScreen;
-import com.inte_soft.gestionconsumibles.util.SplashProceso;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+import javax.enterprise.deploy.spi.exceptions.ConfigurationException;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.swing.SwingUtilities;
+
+
 
 
 /**
@@ -22,6 +29,23 @@ public class GestionConsumibles {
 
     
     public static void main(String[] args) {
+        
+        String configFile = "src\\config.properties";
+          Properties properties = new Properties();
+        try (FileInputStream input = new FileInputStream(configFile)) {
+            properties.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        // Establecer las propiedades del persistence.xml
+        System.setProperty("config.database.url", properties.getProperty("database.url"));
+        System.setProperty("config.database.username", properties.getProperty("database.username"));
+        System.setProperty("config.database.password", properties.getProperty("database.password"));
+
+
+    
         SplashScreen splash = new SplashScreen();
         Login login = new Login();
         splash.setVisible(true);
